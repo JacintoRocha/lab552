43c664ccfc20374bc298e6ad50a4849ddb554a23
jacin <jacinto.rocha01@gmail.com>
2020-02-13 14:16:16 -0800

    calis

---
+++ javamaven/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Persona alumno = new Persona("Jacinto",1252102);
+        System.out.println("Nombre: " + alumno.GetNombre());
+        System.out.println("Matricula: " + alumno.GetMatricula());
+    }
+}
---
+++ javamaven/src/main/java/Persona.java
@@ -0,0 +1,20 @@
+public class Persona {
+
+    private String mi_nombre;
+    private int mi_matricula;
+
+    Persona(String nombre, int matricula)
+    {
+        mi_nombre=nombre;
+        mi_matricula=matricula;
+    }
+
+    String GetNombre()
+    {
+        return mi_nombre;
+    }
+    int GetMatricula()
+    {
+        return mi_matricula;
+    }
+}
