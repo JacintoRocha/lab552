bd17c5dfe7170df11da8e716dc795705fcd8d944
jacin <jacinto.rocha01@gmail.com>
2020-02-13 13:36:37 -0800

    este

---
+++ practica2/src/com/prueba/Persona.java
@@ -0,0 +1,22 @@
+package com.prueba;
+
+public class Persona {
+
+    private String mi_nombre;
+    private int mi_matricula;
+
+    Persona(String nombre, int matricula)
+    {
+        mi_nombre=nombre;
+        mi_matricula=matricula;
+    }
+
+    String GetNombre()
+    {
+        return mi_nombre;
+    }
+    int GetMatricula()
+    {
+        return mi_matricula;
+    }
+}
